5de8115afea74244a260b9e126ee34f47c8ebf92
Michael Dever <mdever@live.ca>
2021-12-01 17:47:45 -0500

    WASD movement

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 // const connect = require('./client');
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +14,18 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === "w") {
+    connection.write('Move: up')
+  }
+  if (key === "s") {
+    connection.write('Move: down')
+  }
+  if (key === "a") {
+    connection.write('Move: left')
+  }
+  if (key === "d") {
+    connection.write('Move: right')
   }
 };
 
