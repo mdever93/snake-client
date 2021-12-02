f0c7f0e71c08cd63d6a3da97e8bfb57a5bd9ad7f
Michael Dever <mdever@live.ca>
2021-12-01 19:25:49 -0500

    constants

---
+++ constants.js
@@ -0,0 +1,20 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const moveKeys = {
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right'
+}
+const msg = {
+  j: 'Say: Gotta go fast',
+  k: "Say: I'm fast as f*** boy",
+  l: 'Say: wackawackawackawacka',
+}
+
+module.exports = {
+  IP,
+  PORT,
+  moveKeys,
+  msg
+};
--- input.js
+++ input.js
@@ -1,5 +1,6 @@
 // const connect = require('./client');
 let connection;
+const { moveKeys, msg } = require('./constants');
 
 const setupInput = function (conn) {
   connection = conn;
@@ -11,31 +12,43 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
     process.exit();
   }
-  if (key === "w") {
-    connection.write('Move: up')
+
+  for (const key of Object.keys(moveKeys)) {
+    if (data === key) {
+      connection.write(moveKeys[key])
+    }
   }
-  if (key === "s") {
-    connection.write('Move: down')
+  for (const key of Object.keys(msg)) {
+    if (data === key) {
+      connection.write(msg[key])
+    }
   }
-  if (key === "a") {
-    connection.write('Move: left')
-  }
-  if (key === "d") {
-    connection.write('Move: right')
-  }
-  if (key === "j") {
-    connection.write('Say: Gotta go fast')
-  }
-  if (key === "k") {
-    connection.write("Say: I'm fast as f*** boy")
-  }
-  if (key === "l") {
-    connection.write('Say: wackawackawackawacka')
-  }
+
+  // if (key === "w") {
+  //   connection.write('Move: up')
+  // }
+  // if (key === "s") {
+  //   connection.write('Move: down')
+  // }
+  // if (key === "a") {
+  //   connection.write('Move: left')
+  // }
+  // if (key === "d") {
+  //   connection.write('Move: right')
+  // }
+  // if (key === "j") {
+  //   connection.write('Say: Gotta go fast')
+  // }
+  // if (key === "k") {
+  //   connection.write("Say: I'm fast as f*** boy")
+  // }
+  // if (key === "l") {
+  //   connection.write('Say: wackawackawackawacka')
+  // }
 };
 
 module.exports = setupInput;
